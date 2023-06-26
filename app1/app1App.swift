5081a1f9fc863aecd09669bfb58534e8f61cd7c5
TCLin <davidzero1107@gmail.com>
2023-06-25 22:27:32 -0500

    Initial Commit

---
+++ app1/app1App.swift
@@ -0,0 +1,17 @@
+//
+//  app1App.swift
+//  app1
+//
+//  Created by 林子欽 on 2023/6/25.
+//
+
+import SwiftUI
+
+@main
+struct app1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
